f374099e72117c4408b534d288417daf78edff16
Anmol Pandit <anmolpandit996@gmail.com>
2021-05-30 00:53:14 -0500

    add edge cases validation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -17,6 +17,7 @@ const tripsPlanListEle = document.querySelector(".my-trip");
 let coords ={originLat:'',originLong:'',destinationLat:'',destinationLong:''};
 
 async function getData(searchPlaceKey) {
+   
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchPlaceKey}.json?bbox=-97.325875,49.766204,-96.953987,49.99275&access_token=${geocodingApiKey}`;
 
   const response = await fetch(url);
@@ -29,12 +30,30 @@ async function getTrips(tripPlanningApiKey) {
   
   let date = new Date().toLocaleDateString();
   let time = new Date().toLocaleTimeString("en-GB");
+ 
+  if(coords.originLat===coords.destinationLat&&coords.originLong===coords.destinationLong){
+    return   tripsPlanListEle.innerHTML =
+    '<h1 class="red">Your are at your Destination</h1>';
+  }
+  
   let url = `https://api.winnipegtransit.com/v3/trip-planner.json?api-key=${tripPlanningApiKey}&origin=geo/${coords.originLat},${coords.originLong}&destination=geo/${coords.destinationLat},${coords.destinationLong}&date=${date}&time=${time}&mode=arrive-before`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+   try{
+    const response = await fetch(url);
+    const data = await response.json();
+    if(data===undefined){
+      return   tripsPlanListEle.innerHTML =
+    '<h1 class="red">Sorry! Cannot process your request at this time.</h1>';
+    
+    }
 
-  return data;
+    return data;
+   }catch(error){
+     console.log(error);
+     tripsPlanListEle.innerHTML =
+    '<h1 class="red">Cannot find your locations, please validate your Origin and Destination location.</h1>';
+    }
+
 }
 
 function renderList(dataArray, listElement) {
@@ -57,10 +76,9 @@ function renderList(dataArray, listElement) {
 
 function renderTrips(tripsPlansArray) {
   tripsPlanListEle.innerHTML = "";
-
   if (tripsPlansArray.length < 1) {
     tripsPlanListEle.innerHTML =
-      '<h1 class="red">No trips are available</h1>';
+      '<h1 class="red">No trips are available at this time.</h1>';
   }
    else{
      addTripHTML(tripsPlansArray);
@@ -174,10 +192,14 @@ function addSelectClass(event) {
   }
 }
 
-planMyTripButtonEle.addEventListener("click", (event) => {
+planMyTripButtonEle.addEventListener("click", (event) => { 
   event.preventDefault();
   const tripPlans = getTrips(tripPlanningApiKey)
-  .then((tripPlans) => {
+  .then((tripPlans) => { 
+    if(tripPlans.plans===undefined){ 
+      return   tripsPlanListEle.innerHTML =
+      '<h1 class="red">Unable to find routes, Try Again with valid inputs!!!</h1>';
+     }
     renderTrips(tripPlans.plans);
   });
 });
